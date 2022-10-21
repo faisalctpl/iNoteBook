893a49a938c114ccc1282de3479c0bf3906a6789
faisal <Faisal@faisalkhan190162.com>
2022-10-21 22:04:09 +0500

    deploy my react app to github pages

---
+++ src/components/About.js
@@ -0,0 +1,54 @@
+import React, { } from 'react'
+
+const About = () => {
+
+  return (
+    <>
+    <div>
+      <h1>Your iNoteBook On The Cloud </h1>
+      <hr />
+      <p>An online web platform where you can create, edit, upload, delete your notes/information privately and securely without any disturbancee.</p>
+
+
+
+      <h6>Codrut Nistor - Senior Tech Writer - 5667 articles published on iNotebookcheck since 2013</h6>
+      <p>In my early school days, I hated writing and having to make up stories. A decade later, I started to enjoy it. Since then, I published a few offline articles and then I moved to the online space, where I contributed to major websites that are still present online as of 2021 such as Softpedia, Brothersoft, Download3000, but I also wrote for multiple blogs that have disappeared over the years. I've been riding with the Notebookcheck crew since 2013 and I am not planning to leave it anytime soon. In love with good mechanical keyboards, vinyl and tape sound, but also smartphones, streaming services, and digital art.
+contact me via:  @online_digi,  online.digital.craft,  LinkedI</p>
+
+<h2>iNotebook Library</h2>
+<p>The notebook library, probably the most complete existing collection of that kind, contains thousands of reviews in several languages which we could find. Our collection currently counts ...
+
+» 11601 reviews from 2022, 15025 from 2021, 14939 from 2020, ...
+→ 138173 reviews in our database</p>
+
+   </div>
+  <div className="container">
+    
+  <h1>Working For iNotebook </h1>
+iNotebook is looking for a couple of highly motivated tech writers who are based in Australia/New Zealand/Oceania.
+As an independent freelancer, you can work wherever and whenever you want. For the most part, you only need a computer and access to the internet.
+
+<h2> Translators </h2>
+
+<h5> Task </h5> 
+
+iNotebook translators translate reviews and other articles at home and put the translated content in our Typo3 CMS. Deadlines do not exist but the content should be available as fast as possible as the topics soon become dated.
+
+Logically, this system applies only to language sections, which we already maintain. If you want to translate to Polish, we will refer you to the licensee who maintains this language section.
+
+The use of automatic translation software is prohibited, even as a support tool.
+
+<h5> Payment </h5> 
+
+We pay translators a pre-determined rate per source word or flat rate per translated article. Contact us for the current price for a specific language.
+
+<h5> Application </h5> 
+
+Curriculum vitae, references, a description of why you are suitable for the position, and a sample review/text (optional).
+
+  </div>
+   </>
+  )
+}
+
+export default About
